0256bab4f45ca07ca739194a11bd3d3b45f7682e
Emilio Freitas <99411296+FreitinhasDuSenai@users.noreply.github.com>
2022-09-06 16:03:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let cata = document.getElementById("cat-a");
+let catb = document.getElementById("cat-b");
+let hipc = document.getElementById("hip-c");
+let out = document.getElementById("result");
+
+let solver = (a, b, c) => {
+    return Math.sqrt(c - b) 
+    || Math.sqrt(c - a) 
+    || Math.sqrt(a + b) 
+    || "Não consigo calcular...";
+}
+
+function result() {
+    let a = Number(cata.value) ** 2 || NaN;
+    let b = Number(catb.value) ** 2 || NaN;
+    let c = Number(hipc.value) ** 2 || NaN;
+    out.innerHTML = (a + b == c)
+     ? `${a} + ${b} = ${c}`: (!a || !b || !c) 
+     ? solver(a, b, c):
+      `A conta está errada...`;
+}
+
+cata.addEventListener("input", result);
+catb.addEventListener("input", result);
+hipc.addEventListener("input", result);
